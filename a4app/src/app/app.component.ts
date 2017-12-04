30f17fcc3f71a706a7d78494fcde7b4a10ac8ce5
Padraic Pairceir <padraic.pairceir@aquaq.co.uk>
2017-12-04 23:27:49 +0000

    Set title from app.component.ts and tidy app.component.html

--- a4app/src/app/app.component.ts
+++ a4app/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Title }     from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+
+  appTitle:string = "App Title";   
+  title = this.appTitle;
+  
+  constructor(private titleService:Title) {
+    this.titleService.setTitle(this.appTitle);
+  }
+    
 }
